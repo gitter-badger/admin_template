89c66c16c2dbc4aa223c7cbf057b0d6646a36a6a
jbarnobal <behind.red.ice@gmail.com>
2018-10-26 13:32:03 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+</body>
+	<header>
+	<!-- Header -->
+		<h1>Simple header</h1>
+	</header>
+	
+	<aside>
+	<!-- Didebar-->	
+		<nav>
+			<ul>
+				<li><a href="#">links 1</a></li>
+				<li><a href="#">links 2</a></li>
+				<li><a href="#">links 3</a></li>
+			</ul>
+		</nav>
+	</aside>
+	<main>
+	<!-- Content -->
+		<h2>Main content</h2>	
+	</main>
+
+	<footer>
+	<!-- footer -->	
+	</footer>
+
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/custom.js"></script>
+</html>
