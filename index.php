17da154d30fc4bd3e518dada74be09b5bc27da0e
jbarnobal <behind.red.ice@gmail.com>
2018-10-27 20:36:26 +0800

    add chart background

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Admin</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 </head>
@@ -36,25 +37,85 @@
 			</div>
 			<div class="row">
 				<div class="column col-6">
-					<div class="card">
-						<span>Column</span>
-						
+					<div id="line-chart" class="card cyan darken-1">
+					<div class="card-content">
+							<h1>This card sample for chart</h1>
+							<span>Column 1</span>	
+							<canvas id="myChart" width="400" height="200"></canvas>
+						</div>		
 					</div>
 				</div>
 				<div class="column col-6">
 					<div class="card">
-						<span>Column</span>
+						<div class="card-content">
+							<h1>This card sample for chart</h1>
+							<span>Column 2</span>	
+						</div>
+						
 					</div>
 				</div>
 			</div>
+			<canvas id="myChart" width="400" height="400"></canvas>
 		</div>
+
 	</main>
 
 	<footer>
 	<!-- footer -->	
 	</footer>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+	<script src="assets/js/chart-min.js"></script>
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
+
 	<script src="assets/js/custom.js"></script>
-	<script src="assets/js/line-chart.js"></script>
+	<script>
+		var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+		    type: 'line',
+		    data: {
+		        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		        datasets: [{
+		            label: 'Number of Votes',
+		            data: [12, 5, 3, 5, 2, 3],
+		            backgroundColor: [
+		            'rgba(255, 255, 255, 0.44)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+		            ],
+		            borderColor: [
+		            'rgba(255, 255, 255, 0.87)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderWidth: 5
+		        }]
+		    },
+		    options: {
+		    	legend:{labels: {fontColor: 'white'}},
+		    	title: {
+		    		display: true,
+		    		fontColor: 'white',
+		    		tet: 'Custom Chart Title'
+		    	},
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true,
+	                    fontColor: 'white'
+	                }
+	            }],
+	            xAxes: [{
+	            	ticks: {
+	            		fontColor: 'white'
+	            	}
+	            }]
+	        }
+		    }
+		});
+	</script>
 </html>
